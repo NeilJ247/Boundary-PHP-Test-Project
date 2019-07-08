bdeeb70d62bcd246e1d67eca60ede4eaf5ad51a3
Neil Jarrett <neil-jarrett@hotmail.co.uk>
2019-07-08 21:27:20 +0100

    Add test to check that Api Exceptions are being caught and returned in the JSON response.

--- tests/Functional/ProductControllerTest.php
+++ tests/Functional/ProductControllerTest.php
@@ -34,4 +34,56 @@ class ProductControllerTest extends BaseTestCase
 
         $this->assertEquals(StatusCode::HTTP_CREATED, $response->getStatusCode());
     }
+
+    /**
+     * @dataProvider invalidDataProvider
+     *
+     * @param array $requestData
+     */
+    public function testPostActionErrors(array $requestData): void
+    {
+        $data = [
+            'data' => $requestData
+        ];
+        $response = $this->runApp('POST', '/api/products', $data);
+
+        $this->assertEquals(StatusCode::HTTP_BAD_REQUEST, $response->getStatusCode());
+
+        ["errors" => $errors] = json_decode($response->getBody(), true);
+
+        $expectedKeys = [
+            'id',
+            'status',
+            'code',
+            'title',
+            'detail',
+            'source',
+        ];
+
+        foreach ($errors as $error) {
+            $keys = array_keys($error);
+            $this->assertSame($expectedKeys, $keys);
+        }
+    }
+
+    public function invalidDataProvider(): array
+    {
+        return [
+            [
+                ["display_name" => "", "cost" => "1.00"],
+            ],
+            [
+                ["display_name" => "PROD 1", "cost" => ""],
+            ],
+            [
+                ["display_name" => "", "cost" => ""],
+            ],
+            [
+                ["display_name" => null, "cost" => null],
+            ],
+            [
+                ["display_name" => "PROD 1", "cost" => "1.e"],
+            ],
+        ];
+    }
 }
